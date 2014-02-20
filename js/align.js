5a856eb0214be8bac32cd86a5e6d8081d13c5471
bdharva <bdharva@alum.mit.edu>
2014-02-20 00:29:29 -0500

    Initial commit

---
+++ js/align.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    align();
+});
+
+window.onresize = function(event) {
+    align();
+}
+
+function align() {
+    window_height = $(window).height();
+    window_width = $(window).width();
+    container_height = $('#container').height()+100;
+    margin = (window_height - container_height)/2;
+    if (margin < 50){margin = 50;}
+    if (window_width < 501){margin = 0;}
+    $('#container').css({'margin-top': margin + 'px'});
+}
