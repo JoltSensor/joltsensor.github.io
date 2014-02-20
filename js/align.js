24894be6263efe1b833dda7d9e6d0d86274740b7
bdharva <bdharva@alum.mit.edu>
2014-02-20 15:44:53 -0500

    Fixed scroll/alignment issue

--- js/align.js
+++ js/align.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 	align();
 });
 
+window.onresize = function(event) {
+    align();
+}
+
 function align() {
     window_height = $(window).height();
     window_width = $(window).width();
@@ -9,5 +13,5 @@ function align() {
     margin = (window_height - container_height)/2;
     if (margin < 50){margin = 50;}
     if (window_width < 501){margin = 0;}
-    $('#container').css({'margin-top': margin + 'px'});
+    $('#container').css({'top': margin + 'px'});
 }
