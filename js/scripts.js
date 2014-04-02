f6177b138ad85d104e40305a89c0b4e8e83b1b8f
bdharva <bdharva@alum.mit.edu>
2014-04-02 07:54:59 -0400

    Split scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,9 @@
 $(document).ready(function(){
-	//align();
 	scroller();
-	//fader();
+	fader();
 });
 
-window.onresize = function(event) {
-    //align();
-}
-
-function align() {
-    window_height = $(window).height();
-    window_width = $(window).width();
-    container_height = $('#container').height()+100;
-    margin = (window_height - container_height)/2;
-    if (margin < 50){margin = 50;}
-    if (window_width < 501){margin = 0;}
-    $('#container').css({'top': margin + 'px'});
-}
+window.onresize = function(event) {}
 
 function scroller() {
 	$(".button").click(function(){
