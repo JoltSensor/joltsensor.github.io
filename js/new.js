d8591611e58e51976282d62d50bf41a6700fe9ac
bdharva <bdharva@alum.mit.edu>
2014-05-19 22:37:54 -0400

    New design in testing

---
+++ js/new.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$("#show_form").click(function(){
+		$("#slide_me").animate({
+			top: "+=800"
+		}, 500);
+		$("#mce-EMAIL").focus();
+	});
+	$("#hide_form").click(function(){
+		$("#slide_me").animate({
+			top: "-=800"
+		}, 500 );
+		$("#mce-EMAIL").val("");
+	});
+});
