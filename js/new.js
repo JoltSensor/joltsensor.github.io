88801c0ec2580e41b12782a3db73558bc7fc9008
bdharva <bdharva@alum.mit.edu>
2014-05-19 22:41:57 -0400

    Killed auto-focus script

--- js/new.js
+++ js/new.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 		$("#slide_me").animate({
 			top: "+=800"
 		}, 500);
-		$("#mce-EMAIL").focus();
+		/*$("#mce-EMAIL").focus();*/
 	});
 	$("#hide_form").click(function(){
 		$("#slide_me").animate({
