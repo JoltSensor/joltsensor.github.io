7e1e1bcb3d100193867653097ebefd4c9652b64b
bdharva <bdharva@alum.mit.edu>
2014-09-01 22:50:15 -0400

    Fixed scrolling issues for screen div larger than screen

--- js/kickstarter.js
+++ js/kickstarter.js
@@ -12,6 +12,11 @@ window.onresize = function(event) {
 function align() {
     window_height = $(window).height();
     container_height = $('.shell').height();
-    margin = (window_height - container_height)/2;
-    $('.shell').css({'top': margin + 'px'});
+    if(window_height < container_height + 40){
+    	$('.shell').css({'top': 0 + 'px'});
+    	$('.shell').css({'padding': '20px 0'});
+    }else{
+	    margin = (window_height - container_height)/2;
+	    $('.shell').css({'top': margin + 'px'});
+	}
 }
