61578fe820243a86ec27c32c3e819bef76b6e8c4
bdharva <bdharva@alum.mit.edu>
2015-04-16 17:42:11 -0500

    Removed slide and fade functions, implemented new parallax scroll with content fade

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,58 @@
 $(document).ready(function(){
     
-    // FADE EFFECT
+    setup();
 
-    /*
-    $('.slide').hide().eq(0).show();
-
-    var pause = 5000;
-    var motion = 3000;
-
-    var slides= $('.slide');
-    var count = slides.length;
-    var i = 0;
-
-    setTimeout(transition, pause);
-
-    function transition(){
-        slides.eq(i).animate({opacity:'toggle'}, motion);
-
-        if(++i>=count){
-            i=0;
-        }
-
-        slides.eq(i).animate({opacity:'toggle'}, motion);
-        
-        setTimeout(transition, pause);
-    }
-    */
-
-    // SLIDE EFFECT
-    // Add something to fix shit getting fucked on resize
-
-    var width = $(window).width();
-
-    $('.slide').animate({left:width});
-    $('.slide').eq(0).animate({left:0});
-
-    var pause = 4000;
-    var motion = 1000;
-
-    var slides= $('.slide');
-    var count = slides.length;
-    var i = 0;
-
-    setTimeout(transition, pause);
-
-    function transition(){
-        slides.eq(i).animate({left:-width}, motion);
-
-        if(i+1>=count){
-            slides.eq(0).animate({left:0}, motion);
-        }else{
-            slides.eq(i+1).animate({left:0}, motion);
-        }
-
-        slides.eq(i).animate({opacity:'toggle'});
-        slides.eq(i).animate({left:width});
-        slides.eq(i).animate({opacity:'toggle'});
-
-        if(++i>=count){
-            i=0;
-        }
-        
-        setTimeout(transition, pause);
-    }
+    // Fixes issues with rendering in some mobile browsers
+    
+    setTimeout(function(){setup();}, 3000);
 
 });
+
+$(window).scroll(function () {
+
+    var height = $('#hero').height();
+    var scrolled = $(window).scrollTop();
+    var difference = height - scrolled;
+
+    if(difference > 60){
+
+        $('#header_wrapper').css({'background-color' : "rgba(26,102,216,0)"});
+        $('#header_Wrapper').css({'box-shadow' : '0 2px 5px rgba(0,0,0,0)'});
+
+    }else {
+
+        $('#header_wrapper').css({'background-color' : "rgba(26,102,216,1)"});
+        $('#header_Wrapper').css({'box-shadow' : '0 2px 5px rgba(0,0,0,0.26)'});
+
+    }
+
+    // Parallax scrolling for hero
+
+    $('#hero').css({'top' : (-scrolled/2)+"px"});
+
+    // Fade out words on hero with parallax
+
+    $('.content').css({'opacity': 1-(1.5*scrolled/height)});
+
+});
+
+function setup() {
+
+    var height = $('#hero').height();
+
+    // Push the wrapper past the bottom of the screen
+
+    $('#wrapper').css({'top': height+"px"});
+
+    // Fill the screen with the hero div
+
+    $('#hero').css({'height': height+"px"});
+
+    // Center hero text block on hero
+
+    var text_height = $('.content').height();
+    var text_margin = (height - text_height) / 2;
+
+    $('.content').css({'margin-top': text_margin+"px"});
+    $('.content').css({'margin-bottom': text_margin+"px"});
+}
