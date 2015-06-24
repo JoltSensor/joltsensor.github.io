d9ebe774a34ebab9065d5854f3a2f61320405f66
bdharva <bdharva@alum.mit.edu>
2015-06-24 18:06:16 -0500

    Reworked header & footer, implemented copy edit suggestions from T3, added popup HelloBar mailing list sign-up, fixed feed.xml generation with empty YAML header

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
     // Fixes issues with rendering in some mobile browsers
     
     setTimeout(function(){setup();}, 1000);
-    setTimeout(function(){setup();}, 2000);
 
 });
 
@@ -23,7 +22,7 @@ $(window).scroll(function () {
 
         // Parallax scrolling for hero
 
-        $('#hero').css({'top' : (60-scrolled/2)+"px"});
+        $('#hero').css({'top' : 70 - (scrolled/2)+"px"});
 
         // Stop any other animations affecting hero content & fade out with scroll
         
@@ -33,52 +32,75 @@ $(window).scroll(function () {
 
 });
 
+$('.hamburger').click(function() {
+
+    $('#menu').fadeToggle(10, function() {
+
+        $('.hamburger').hide();
+        $('.exit').show();
+
+    });
+
+});
+
+$('.exit').click(function() {
+
+    $('#menu').fadeToggle(10, function() {
+    
+        $('.exit').hide();
+        $('.hamburger').show();
+    
+    });
+
+});
+
 function setup() {
 
-    $('#hero.full').css({'height' : $(window).height() - 60 + "px"});
+    $('.hamburger').show();
+    $('.exit').hide().css({'visibility' : 'visible'});
+    $('#menu').hide().css({'visibility' : 'visible'});
+    $('#menu').css({'height' : $(window).height() - 70 + "px"});
 
     if($(window).height() < 600){
-        $('#hero').css({'height' : $(window).height() - 60 + "px"});
+
+        $('#hero').css({'height' : $(window).height() - 70 + "px"});
+    
     }else{
+    
         $('#hero').css({'height' : 500 + "px"});
+    
     };
 
     if($(window).width() < 480){
-        $('#hero').css({'height' : $(window).height() - 60 + "px"});
+    
+        $('#hero').css({'height' : $(window).height() - 70 + "px"});
+    
     }else{
+    
         $('#hero').css({'height' : 500 + "px"});
+    
     };
+
+    $('#hero.full').css({'height' : $(window).height() - 140 + "px"});
 
     var height = $('#hero').height();
 
     // Push the wrapper past the bottom of the hero
 
-    $('#wrapper').css({'top': 60+height+"px"});
+    $('#wrapper').css({'top': 70 + height+"px"});
 
     // Center hero content vertically
 
     var text_height = $('.content').height();
     var text_margin = (height - text_height) / 2;
 
-    $('#hero .content').css({'margin-top': text_margin+"px"});
-    $('#hero .content').css({'margin-bottom': text_margin+"px"});
+    $('#hero .content').css({'margin-top': text_margin +"px"});
+    $('#hero .content').css({'margin-bottom': text_margin +"px"});
 
     // Fade hero content proper amount if naviagating back to or refreshing a partially scrolled page
 
     var scrolled = $(window).scrollTop();
 
-    $('#hero .content').animate({opacity : 1-(1.5*scrolled/height)}, 2000);
-    $('.thumbnail .content').animate({opacity : 1}, 2000);
+    $('#hero .content').css({'opacity' : 1-(1.5*scrolled/height)});
 
-    // Content alignment for each blog thumbnail element
-
-    $('.thumbnail').each(function( index ){
-
-        var height = $('.thumbnail').children('.screen').height();
-        var text_height = $(this).children('.screen').children('.content').height();
-        var text_margin = (height - text_height) / 2;
-        $(this).children('.screen').children('.content').css({'margin-top': text_margin+"px"});
-        $(this).children('.screen').children('.content').css({'margin-bottom': text_margin+"px"});
-
-    })
 };
