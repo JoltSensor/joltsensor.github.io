d77981bdc77223724648400e2bd3288925adb287
bdharva <bdharva@alum.mit.edu>
2015-06-25 01:51:23 -0500

    Lots of little tweaks before pushing

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
     
     setup();
 
-    // Fixes issues with rendering in some mobile browsers
-    
     setTimeout(function(){setup();}, 1000);
 
 });
@@ -20,12 +18,7 @@ $(window).scroll(function () {
         var scrolled = $(window).scrollTop();
         var difference = height - scrolled;
 
-        // Parallax scrolling for hero
-
         $('#hero').css({'top' : 70 - (scrolled/2)+"px"});
-
-        // Stop any other animations affecting hero content & fade out with scroll
-        
         $('#hero .content').stop().css({'opacity': 1-(1.5*scrolled/height)});
     
     }
@@ -74,10 +67,19 @@ function setup() {
     if($(window).width() < 480){
     
         $('#hero').css({'height' : $(window).height() - 70 + "px"});
+        $('#hero').css({'top' : 70 +"px"});
+        $('#hero .content').css({'opacity': 1});
     
     }else{
     
         $('#hero').css({'height' : 500 + "px"});
+
+        var height = $('#hero').height();
+        var scrolled = $(window).scrollTop();
+        var difference = height - scrolled;
+
+        $('#hero').css({'top' : 70 - (scrolled/2)+"px"});
+        $('#hero .content').stop().css({'opacity': 1-(1.5*scrolled/height)});
     
     };
 
@@ -85,22 +87,12 @@ function setup() {
 
     var height = $('#hero').height();
 
-    // Push the wrapper past the bottom of the hero
-
     $('#wrapper').css({'top': 70 + height+"px"});
-
-    // Center hero content vertically
 
     var text_height = $('.content').height();
     var text_margin = (height - text_height) / 2;
 
     $('#hero .content').css({'margin-top': text_margin +"px"});
     $('#hero .content').css({'margin-bottom': text_margin +"px"});
-
-    // Fade hero content proper amount if naviagating back to or refreshing a partially scrolled page
-
-    var scrolled = $(window).scrollTop();
-
-    $('#hero .content').css({'opacity' : 1-(1.5*scrolled/height)});
 
 };
