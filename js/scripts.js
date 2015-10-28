85ecdd035f2e811b8dba5ccbaaed9023028c4013
bdharva <bdharva@alum.mit.edu>
2015-10-27 22:10:21 -0400

    Design overhaul

--- js/scripts.js
+++ js/scripts.js
@@ -8,86 +8,77 @@ $(document).ready(function(){
 
 $(window).resize(setup);
 
+$("#learnmore").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#more").offset().top
+    }, 1000);
+});
+
 $('.hamburger').click(function() {
 
-    $('#menu').fadeToggle(10, function() {
+    $('.hamburger').hide();
+    $('.exit').show();
 
-        $('.hamburger').hide();
-        $('.exit').show();
-        
-        var height = $(window).height();
-        var width = $(window).width();
-        var space = ((width-1000)/2)-20;
+    var width = $(window).width();
+    var space = ((width-1000)/2)-20;
 
-        if (space > 280) {
+    if (space > 280) {
 
-            $('#menu').css({'width' : space + 'px'});
+        $('#menu').animate({right : 0},200);
 
-        } else if (space >= 0) {
-            
-            $('#menu').css({'width' : 280 + 'px'});
-            $('#wrapper').css({'right' : (280-space) + 'px'});
-            $('#header').css({'right' : (280-space) + 'px'});
-        
-        } else {
+    } else if (space >= 0) {
 
-            $('#menu').css({'width' : 280 + 'px'});
-            $('#wrapper').css({'right' : 280 + 'px'});
-            $('#header').css({'right' : 280 + 'px'});
+        $('#menu').animate({right : 0},200);
+        $('#wrapper').animate({left : space-280},200);
 
-        }
+    } else {
 
-    });
+        $('#menu').animate({right : 0},200);
+        $('#wrapper').animate({left : -280},200);
+
+    }
 
 });
 
 $('.exit').click(function() {
 
-    $('#menu').fadeToggle(10, function() {
-    
-        $('.exit').hide();
-        $('.hamburger').show();
-        $('#wrapper').css({'right' : '0'});
-        $('#header').css({'right' : '0'});
-    
-    });
+    menu_hide(200);
 
 });
+
+
+function menu_hide(delay) {
+
+    $('.exit').hide();
+    $('.hamburger').show();
+
+    $('#menu').animate({right : -280},delay);
+    $('#header').animate({left : 0},delay);
+    $('#wrapper').animate({left : 0},delay);
+
+};
 
 function setup() {
 
     $('.hamburger').show();
     $('.exit').hide().css({'visibility' : 'visible'});
-    $('#menu').hide().css({'visibility' : 'visible'});
 
-    if($(window).height() < 600){
+    if($(window).height() < 600 || $(window).width() < 480){
 
-        //$('#hero').css({'height' : $(window).height() - 70 + "px"});
+        $('#hero.full').css({'height' : $(window).height() + "px"});
     
     }else{
     
-        $('#hero').css({'height' : 400 + "px"});
-    
-    };
+        $('#hero.full').css({'height' : $(window).height() - ($('.coverage').height() + 60) + "px"});
 
-    if($(window).width() < 480){
-    
-        //$('#hero').css({'height' : $(window).height() - 70 + "px"});
-    
-    }else{
-    
-        $('#hero').css({'height' : 400 + "px"});
-    
-    };
-
-    $('#hero.full').css({'height' : $(window).height() - 70 + "px"});
+    }
 
     var height = $('#hero').height();
     var text_height = $('.content').height();
     var text_margin = (height - text_height) / 2;
 
     $('#hero .content').css({'opacity': 1});
-    $('#hero .content').css({'margin-top': text_margin +"px"});
+    $('#hero .content').css({'margin-top': 20 + text_margin +"px"});
 
     $('.section.hero').each(function() {
     
@@ -96,6 +87,16 @@ function setup() {
         var margin = ( height1 - height2 )/2;
 
         $(this).find('.screen > .content').css({'padding-top' : margin});
+
+    });
+
+    $('.blog .screen').each(function() {
+    
+        var height1 = $(this).height();
+        var height2 = $(this).find('.content').height();
+        var margin = ( height1 - height2 )/2;
+
+        $(this).find('.content').css({'padding-top' : margin});
 
     });
 
